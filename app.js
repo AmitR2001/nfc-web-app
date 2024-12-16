8fef0d555f46ebac978bdd64ffb57ea788442fdb
Amit Manohar Rathod <amit.rathod@spit.ac.in>
2024-12-16 10:30:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
+// document.getElementById('writeButton').addEventListener('click', async () => {
+//     const name = document.getElementById('nameInput').value;
+//     if ('NDEFWriter' in window) {
+//         try {
+//             const ndef = new NDEFWriter();
+//             await ndef.write({ records: [{ recordType: "text", data: name }] });
+//             document.getElementById('message').textContent = 'Name written to NFC tag!';
+//         } catch (error) {
+//             document.getElementById('message').textContent = `Error: ${error}`;
+//         }
+//     } else {
+//         document.getElementById('message').textContent = 'Web NFC is not supported on this device.';
+//     }
+// });
+
 document.getElementById('writeButton').addEventListener('click', async () => {
     const name = document.getElementById('nameInput').value;
-    if ('NDEFWriter' in window) {
+    if ('NDEFReader' in window) {
         try {
-            const ndef = new NDEFWriter();
-            await ndef.write({ records: [{ recordType: "text", data: name }] });
-            document.getElementById('message').textContent = 'Name written to NFC tag!';
+            const ndef = new NDEFReader();
+            await ndef.write("Hello World").then(() => {
+                document.getElementById('message').textContent = 'Message written.';
+            }).catch(error => {
+                document.getElementById('message').textContent = `Write failed :-( try again: ${error}.`;
+            });
         } catch (error) {
             document.getElementById('message').textContent = `Error: ${error}`;
         }
     } else {
-        console.log('NDEFWriter is not available in window');
         document.getElementById('message').textContent = 'Web NFC is not supported on this device.';
     }
 });
@@ -29,7 +46,6 @@ document.getElementById('readButton').addEventListener('click', async () => {
             document.getElementById('message').textContent = `Error: ${error}`;
         }
     } else {
-        console.log('NDEFReader is not available in window');
         document.getElementById('message').textContent = 'Web NFC is not supported on this device.';
     }
 });
